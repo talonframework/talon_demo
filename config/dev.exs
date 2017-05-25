ada3b11d5d40a95d6edc026881dce336211b98f0
Ninmesara <cryptonomicon314@gmail.com>
2017-05-25 15:28:21 +0100

    Fixed brunch watcher on Windows

--- config/dev.exs
+++ config/dev.exs
@@ -24,7 +24,7 @@ config :talon_demo, TalonDemo.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/.bin/brunch", "watch", "--stdin",
+  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
                     cd: Path.expand("../", __DIR__)]]
 
 
